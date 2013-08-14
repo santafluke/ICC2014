993b567b589985fa7c1d4ddc2ed8f2616f29c793
santafluke <santafluke@gmail.com>
2013-08-14 19:13:56 +0800

    14Aug

--- ICC2014/Exp2e_lambdaC.py
+++ ICC2014/Exp2e_lambdaC.py
@@ -3,3 +3,82 @@ Created on Aug 12, 2013
 
 @author: yzhang28
 '''
+from Headers import *
+
+expnum = 11
+lamc_list = [0.00001, 0.0001, 0.0002, 0.0003, 0.0004, 0.0005, 0.0006, 0.0007, 0.0008, 0.0009, 0.001]
+total_cost_list_lamc = [-1 for _ in range(expnum)]
+act_1_rate_list_lamc = [-1 for _ in range(expnum)]
+
+total_cost_list_lamc_alllocal = [-1 for _ in range(expnum)]
+total_cost_list_lamc_allremote = [-1 for _ in range(expnum)]
+total_cost_list_lamc_random = [-1 for _ in range(expnum)]
+total_cost_list_lamc_myopic = [-1 for _ in range(expnum)]
+      
+# print "Ready to start..."
+# for i in range(expnum):    
+#     print "Alive..."
+#     lamc = lamc_list[i]
+#     v_mat, a_mat = MDPOptimization(penalty_input=20.0, lam_c_inp =lamc, setEtaDirectly=True, eta_inp=0.6)
+#     act_1_rate_list_lamc[i], total_N = OffloadingRate(a_mat,getN=True)
+#     total_cost_list_lamc[i] = ValueAverage(v_mat)
+#        
+#     print "   sub-Alive..."
+#     v_mat, a_mat = MDPOptimization(penalty_input=20.0, lam_c_inp =lamc, actionflag_inp='AlwaysLocal',setEtaDirectly=True, eta_inp=0.6)
+#     total_cost_list_lamc_alllocal[i] = ValueAverage(v_mat)
+#     print "   sub-Alive..."
+#     v_mat, a_mat = MDPOptimization(penalty_input=20.0, lam_c_inp =lamc, actionflag_inp='AlwaysRemote',setEtaDirectly=True, eta_inp=0.6)
+#     total_cost_list_lamc_allremote[i] = ValueAverage(v_mat)
+# #     print "   sub-Alive..."
+# #     v_mat, a_mat = MDPOptimization(penalty_input=20.0, lam_c_inp =lamc, actionflag_inp='Random',setEtaDirectly=True, eta_inp=0.6)
+# #     total_cost_list_lamc_random[i] = ValueAverage(v_mat)    
+#     print "   sub-Alive..."
+#     v_mat, a_mat = MDPOptimization(penalty_input=20.0, lam_c_inp =lamc, actionflag_inp='Myopic',setEtaDirectly=True, eta_inp=0.6)
+#     total_cost_list_lamc_myopic[i] = ValueAverage(v_mat) 
+# print "lamc_list=", lamc_list
+# print "total_cost_list_lamc=", total_cost_list_lamc
+# print "total_cost_list_lamc_alllocal=", total_cost_list_lamc_alllocal
+# print "total_cost_list_lamc_allremote=", total_cost_list_lamc_allremote
+# # print "total_cost_list_lamc_random=", total_cost_list_lamc_random
+# print "total_cost_list_lamc_myopic=", total_cost_list_lamc_myopic
+# print "act_1_rate_list_lamc=", act_1_rate_list_lamc
+
+# changed, 10 Aug 2013 
+lamc_list= [1e-05, 0.0001, 0.0002, 0.0003, 0.0004, 0.0005, 0.0006, 0.0007, 0.0008, 0.0009, 0.001]
+total_cost_list_lamc= [112.90219412266396, 108.24085667130383, 107.67088660472957, 103.66786245486333, 103.07536565967428, 102.46711879414364, 101.84388505531878, 101.20613408589234, 98.102016345471284, 97.457917483668723, 96.806491932378094]
+total_cost_list_lamc_alllocal= [115.42793602951228, 115.42788762929932, 115.41435392708919, 115.42867403274538, 115.42617536956514, 115.42106756853111, 115.41206490584807, 115.39769751136929, 115.42720517613014, 115.42518824566829, 115.42214240293013]
+total_cost_list_lamc_allremote= [114.77671675471072, 109.84255056068618, 109.60337947113109, 105.60212231746266, 105.29556232040277, 104.95120347671542, 104.57008623989587, 104.15302910458855, 101.06418235639723, 100.60104068705395, 100.11111248614817]
+total_cost_list_lamc_myopic= [117.21731138383491, 112.61132178654572, 112.87840634326048, 108.84427683567451, 109.00166670600197, 109.10051851108669, 109.14123982880902, 109.12416386788414, 106.05454275816494, 105.94646942723672, 105.78943790072795]
+act_1_rate_list_lamc= [0.3541666666666667, 0.5555555555555556, 0.5555555555555556, 0.65625, 0.65625, 0.65625, 0.65625, 0.65625, 0.7166666666666667, 0.7166666666666667, 0.7166666666666667]
+
+
+figa = plt.figure(figsize=(4.5,4.5))
+grid(True, which="both")
+plot(lamc_list,act_1_rate_list_lamc,color='red',marker='o')
+xlabel('Cloudlet density $\\lambda_c$',fontsize=16)
+ylabel('Offloading rate', fontsize=16)
+subplots_adjust(top=0.93,bottom=0.16,left=0.12, right=0.95)
+# xlim([4,36])
+ylim([0.28,0.75])
+locs, labels = plt.yticks()
+plt.setp(labels, rotation=90)
+
+   
+figc = plt.figure(figsize=(4.5,4.5))
+grid(True, which="both")
+plot(lamc_list,total_cost_list_lamc,color='red',marker='o',label='MDP')
+plot(lamc_list,total_cost_list_lamc_alllocal,color='black',linestyle='--', fillstyle='none', marker='s',label='LOC')
+plot(lamc_list,total_cost_list_lamc_allremote,color='blue',linestyle='--', fillstyle='none', marker='x',label='OFF')
+# plot(lamc_list,total_cost_list_lamc_random,color='cyan',linestyle='--', fillstyle='none', marker='^',label='RND')
+plot(lamc_list,total_cost_list_lamc_myopic,color='green',linestyle='--', fillstyle='none', marker='^',label='MYO')
+xlabel('Cloudlet density $\\lambda_c$',fontsize=16)
+ylabel('User\'s avg. cost', fontsize=16)
+subplots_adjust(top=0.93,bottom=0.16,left=0.12, right=0.95)
+legend(loc='lower left')
+locs, labels = plt.yticks()
+plt.setp(labels, rotation=90)
+# xlim([4,36])
+ylim([93,120])
+   
+show()
+print "TERMINATED."
--- ICC2014/Exp3_avail.py
+++ ICC2014/Exp3_avail.py
@@ -91,71 +91,99 @@ total_cost_list_myopic_highpen = [-1 for _ in range(len(eta_list))]
 # print "total_cost_list_myopic_highpen=", total_cost_list_myopic_highpen #COMPARE, MYOPIC total cost  
 
 
-eta_list= [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-act_1_rate_list[0]= [0.0, 0.6388888888888888, 0.6388888888888888, 0.6388888888888888, 0.6388888888888888, 0.6388888888888888, 0.6388888888888888, 0.6388888888888888, 0.6388888888888888, 0.6388888888888888, 0.6666666666666666]
-act_1_rate_list[1]= [0.0, 0.4722222222222222, 0.4722222222222222, 0.4722222222222222, 0.5555555555555556, 0.5555555555555556, 0.625, 0.6388888888888888, 0.6388888888888888, 0.6388888888888888, 0.6666666666666666]
-act_1_rate_list[2]= [0.0, 0.4722222222222222, 0.4722222222222222, 0.4722222222222222, 0.4722222222222222, 0.5555555555555556, 0.5555555555555556, 0.5972222222222222, 0.6388888888888888, 0.6388888888888888, 0.6666666666666666]
-total_cost_list[0]= [115.42788762929932, 100.38220109999523, 99.857350398502263, 99.367435800408998, 98.912494659725041, 98.492563400672609, 98.107681759706068, 97.757886021843277, 97.443213736101882, 97.163701312850861, 96.91934108412535]
-total_cost_list[1]= [115.42788762929932, 107.03157852956643, 108.30403716209271, 109.48901570608633, 109.33704983994181, 108.27807438877045, 106.7616921240035, 103.92255864627963, 101.29384096383116, 98.95959400213539, 96.91934108412535]
-total_cost_list[2]= [115.42788762929932, 107.39062027805772, 108.60885151706485, 109.74320580732321, 110.79403307692374, 110.53470009524017, 108.97253624433297, 107.74222266849782, 103.8609257823171, 100.15685504920452, 96.91934108412535]
-total_cost_list_alllocal_nopen= [115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932]
-total_cost_list_allremote_nopen= [115.42788762929932, 100.62655567759458, 100.06297630761426, 99.537271457767275, 99.049458948028629, 98.599555772856405, 98.18758235160729, 97.813555759911736, 97.477494454213527, 97.179415870103085, 96.91934108412535]
-total_cost_list_random_nopen= [115.42788762929932, 109.61768286145798, 106.85492055802938, 106.03977991471801, 109.05981115741305, 109.44153555851183, 104.60724893199296, 103.21338929988266, 103.50749487184481, 107.68766055954956, 108.67759300278522]
-total_cost_list_myopic_nopen= [115.42788762929932, 100.62655567759458, 100.06297630761426, 99.537271457767275, 99.049458948028629, 98.599555772856405, 98.18758235160729, 97.813555759911736, 97.477494454213527, 97.179415870103085, 96.91934108412535]
-total_cost_list_alllocal_highpen= [115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932]
-total_cost_list_allremote_highpen= [115.42788762929932, 143.18483392526051, 136.14953444939366, 129.58988110503668, 123.50502117625307, 117.89410650455082, 112.75629261295593, 108.09074224949627, 103.89661972688506, 100.17309488140523, 96.91934108412535]
-total_cost_list_random_highpen= [115.42788762929932, 119.69236794124419, 129.65637573770493, 125.2989527492841, 117.94577925744021, 116.78419469820246, 114.07131252702818, 112.58946656196957, 109.17924972297959, 106.86344592989046, 105.78650135050555]
-total_cost_list_myopic_highpen= [115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 117.1891405867284, 114.19884520938783, 111.75240278279176, 110.01050482221505, 104.951418381422, 100.6995924145365, 96.91934108412535]
+eta_list= [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+act_1_rate_list[0]= [0.6388888888888888, 0.6388888888888888, 0.6388888888888888, 0.6388888888888888, 0.6388888888888888, 0.6388888888888888, 0.6388888888888888, 0.6388888888888888, 0.6388888888888888, 0.6666666666666666]
+act_1_rate_list[1]= [0.4722222222222222, 0.4722222222222222, 0.4722222222222222, 0.5555555555555556, 0.5555555555555556, 0.625, 0.6388888888888888, 0.6388888888888888, 0.6388888888888888, 0.6666666666666666]
+act_1_rate_list[2]= [0.4722222222222222, 0.4722222222222222, 0.4722222222222222, 0.4722222222222222, 0.5555555555555556, 0.5555555555555556, 0.5972222222222222, 0.6388888888888888, 0.6388888888888888, 0.6666666666666666]
+total_cost_list[0]= [100.38220109999523, 99.857350398502263, 99.367435800408998, 98.912494659725041, 98.492563400672609, 98.107681759706068, 97.757886021843277, 97.443213736101882, 97.163701312850861, 96.91934108412535]
+total_cost_list[1]= [107.03157852956643, 108.30403716209271, 109.48901570608633, 109.33704983994181, 108.27807438877045, 106.7616921240035, 103.92255864627963, 101.29384096383116, 98.95959400213539, 96.91934108412535]
+total_cost_list[2]= [107.39062027805772, 108.60885151706485, 109.74320580732321, 110.79403307692374, 110.53470009524017, 108.97253624433297, 107.74222266849782, 103.8609257823171, 100.15685504920452, 96.91934108412535]
+total_cost_list_alllocal_nopen= [115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932]
+total_cost_list_allremote_nopen= [100.62655567759458, 100.06297630761426, 99.537271457767275, 99.049458948028629, 98.599555772856405, 98.18758235160729, 97.813555759911736, 97.477494454213527, 97.179415870103085, 96.91934108412535]
+total_cost_list_random_nopen= [109.61768286145798, 106.85492055802938, 106.03977991471801, 109.05981115741305, 109.44153555851183, 104.60724893199296, 103.21338929988266, 103.50749487184481, 107.68766055954956, 108.67759300278522]
+total_cost_list_myopic_nopen= [100.62655567759458, 100.06297630761426, 99.537271457767275, 99.049458948028629, 98.599555772856405, 98.18758235160729, 97.813555759911736, 97.477494454213527, 97.179415870103085, 96.91934108412535]
+total_cost_list_alllocal_highpen= [115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932, 115.42788762929932]
+total_cost_list_allremote_highpen= [143.18483392526051, 136.14953444939366, 129.58988110503668, 123.50502117625307, 117.89410650455082, 112.75629261295593, 108.09074224949627, 103.89661972688506, 100.17309488140523, 96.91934108412535]
+total_cost_list_random_highpen= [119.69236794124419, 129.65637573770493, 125.2989527492841, 117.94577925744021, 116.78419469820246, 114.07131252702818, 112.58946656196957, 109.17924972297959, 106.86344592989046, 105.78650135050555]
+total_cost_list_myopic_highpen= [115.42788762929932, 115.42788762929932, 115.42788762929932, 117.1891405867284, 114.19884520938783, 111.75240278279176, 110.01050482221505, 104.951418381422, 100.6995924145365, 96.91934108412535]
 
 
 figa = plt.figure(figsize=(4.5,4.5))
 grid(True, which="both")
-plot(eta_list,act_1_rate_list[0],color='black',marker='o', fillstyle='none',label="No penalty")
-plot(eta_list,act_1_rate_list[1],color='blue',marker='s', fillstyle='none', label="Mild penalty")
-plot(eta_list,act_1_rate_list[2],color='red',marker='^', fillstyle='none', label="High penalty")
-subplots_adjust(top=0.93,bottom=0.16,left=0.15, right=0.95)
-xlabel('Cloudlet availability $\\eta_a$')
-ylabel('Rate of offloading decisions')
-legend(loc='lower right',prop={'size':10})
+plot(eta_list,act_1_rate_list[0],color='black',marker='o',label="$c_{pen}$=0")
+plot(eta_list,act_1_rate_list[1],color='blue',marker='s', label="$c_{pen}$=15")
+plot(eta_list,act_1_rate_list[2],color='red',marker='^', label="$c_{pen}$=25")
+subplots_adjust(top=0.93,bottom=0.16,left=0.12, right=0.95)
+xlabel('Cloudlet availability $\\eta_a$', fontsize=16)
+ylabel('Offloading rate', fontsize=16)
+legend(loc='lower right')
+locs, labels = plt.yticks()
+plt.setp(labels, rotation=90)
 # xlim([-0.01,1.01])
-# ylim([0.0,0.75])
+ylim([0.46,0.70])
  
 figb = plt.figure(figsize=(4.5,4.5))
 grid(True, which="both")
-plot(eta_list,total_cost_list[0],color='black',marker='o', fillstyle='none',label="No penalty")
-plot(eta_list,total_cost_list[1],color='blue',marker='s', fillstyle='none', label="Mild penalty")
-plot(eta_list,total_cost_list[2],color='red',marker='^', fillstyle='none', label="High penalty")
-subplots_adjust(top=0.93,bottom=0.16,left=0.15, right=0.95)
-legend(loc='best',prop={'size':10})
-xlabel('Cloudlet availability $\\eta_a$')
-ylabel('User\'s avg. cost')
-# xlim([-0.01,1.01])
+plot(eta_list,total_cost_list[0],color='black',marker='o', label="$c_{pen}$=0")
+plot(eta_list,total_cost_list[1],color='blue',marker='s', label="$c_{pen}$=15")
+plot(eta_list,total_cost_list[2],color='red',marker='^', label="$c_{pen}$=25")
+subplots_adjust(top=0.93,bottom=0.16,left=0.12, right=0.95)
+legend(loc='best')
+xlabel('Cloudlet availability $\\eta_a$', fontsize=16)
+ylabel('User\'s avg. cost', fontsize=16)
+locs, labels = plt.yticks()
+plt.setp(labels, rotation=90)
+ylim([97, 112])
+
+figbextra = plt.figure(figsize=(4.5,4.5))
+grid(True, which="both")
+p1,= plot(eta_list,total_cost_list[0],color='black',marker='o', label="MDP, $c_{pen}$=0")
+p2,= plot(eta_list,total_cost_list[1],color='blue',marker='s', label="MDP, $c_{pen}$=15")
+p3,= plot(eta_list,total_cost_list[2],color='red',marker='^', label="MDP, $c_{pen}$=25")
+p4,= plot(eta_list,total_cost_list_alllocal_highpen,color='black',marker='s', fillstyle='none',linestyle='--',label="LOC,$c_{pen}$=25")
+p5,= plot(eta_list,total_cost_list_allremote_highpen,color='blue',marker='x', fillstyle='none',linestyle='--',label="OFF,$c_{pen}$=25")
+#p6,= plot(eta_list,total_cost_list_random_highpen,color='cyan',marker='v', fillstyle='none',linestyle='--',label="RND,$c_{pen}$=25")
+p7,= plot(eta_list,total_cost_list_myopic_highpen,color='green',marker='^', fillstyle='none',linestyle='--',label="MYO,$c_{pen}$=25")
+subplots_adjust(top=0.93,bottom=0.16,left=0.12, right=0.95)
+legend(loc='best',prop={'size':11})
+# l1 = legend([p1,p2,p3],["MDP, $c_{pen}$=0","MDP, $c_{pen}$=15","MDP, $c_{pen}$=25"],loc='upper right',prop={'size':11})
+# l2 = legend([p4,p5,p7],["LOC, $c_{pen}$=25","OFF, $c_{pen}$=25","MYO, $c_{pen}$=25"],loc='center right',prop={'size':11})
+# gca().add_artist(l1)
+xlabel('Cloudlet availability $\\eta_a$', fontsize=16)
+ylabel('User\'s avg. cost', fontsize=16)
+locs, labels = plt.yticks()
+plt.setp(labels, rotation=90)
+ylim([95, 145])
  
 figc = plt.figure(figsize=(4.5,4.5))
 grid(True, which="both")
-plot(eta_list,total_cost_list[0],color='black',marker='o', fillstyle='none',label="No penalty")
-plot(eta_list,total_cost_list_alllocal_nopen,color='black',marker='v', fillstyle='none',linestyle='--',label="[Compare] LOC")
-plot(eta_list,total_cost_list_allremote_nopen,color='blue',marker='v', fillstyle='none',linestyle='--',label="[Compare] OFF")
-plot(eta_list,total_cost_list_random_nopen,color='green',marker='v', fillstyle='none',linestyle='--',label="[Compare] RND")
-plot(eta_list,total_cost_list_myopic_nopen,color='cyan',marker='v', fillstyle='none',linestyle='--',label="[Compare] MYO")
+plot(eta_list,total_cost_list[0],color='black',marker='o', fillstyle='none',label="MDP")
+plot(eta_list,total_cost_list_alllocal_nopen,color='black',marker='s', fillstyle='none',linestyle='--',label="LOC")
+plot(eta_list,total_cost_list_allremote_nopen,color='blue',marker='x', fillstyle='none',linestyle='--',label="OFF")
+# plot(eta_list,total_cost_list_random_nopen,color='cyan',marker='v', fillstyle='none',linestyle='--',label="RND")
+plot(eta_list,total_cost_list_myopic_nopen,color='green',marker='^', fillstyle='none',linestyle='--',label="MYO")
 # xlim([-0.01,1.01])
-subplots_adjust(top=0.93,bottom=0.16,left=0.15, right=0.95)
-legend(loc='best',prop={'size':10})
-xlabel('Cloudlet availability $\\eta_a$')
-ylabel('User\'s avg. cost')
+subplots_adjust(top=0.93,bottom=0.16,left=0.12, right=0.95)
+legend(loc='best')
+xlabel('Cloudlet availability $\\eta_a$', fontsize=16)
+ylabel('User\'s avg. cost', fontsize=16)
+locs, labels = plt.yticks()
+plt.setp(labels, rotation=90)
 
 figd = plt.figure(figsize=(4.5,4.5))
 grid(True, which="both")
-plot(eta_list,total_cost_list[2],color='red',marker='^', fillstyle='none',label="High penalty")
-plot(eta_list,total_cost_list_alllocal_highpen,color='black',marker='v', fillstyle='none',linestyle='--',label="[Compare] LOC")
-plot(eta_list,total_cost_list_allremote_highpen,color='blue',marker='v', fillstyle='none',linestyle='--',label="[Compare] OFF")
-plot(eta_list,total_cost_list_random_highpen,color='green',marker='v', fillstyle='none',linestyle='--',label="[Compare] RND")
-plot(eta_list,total_cost_list_myopic_highpen,color='cyan',marker='v', fillstyle='none',linestyle='--',label="[Compare] MYO")
-# xlim([-0.01,1.01])
-subplots_adjust(top=0.93,bottom=0.16,left=0.15, right=0.95)
-legend(loc='best',prop={'size':10})
-xlabel('Cloudlet availability $\\eta_a$')
-ylabel('User\'s avg. cost')
+plot(eta_list,total_cost_list[2],color='red',marker='^', fillstyle='none',label="MDP")
+plot(eta_list,total_cost_list_alllocal_highpen,color='black',marker='s', fillstyle='none',linestyle='--',label="LOC")
+plot(eta_list,total_cost_list_allremote_highpen,color='blue',marker='x', fillstyle='none',linestyle='--',label="OFF")
+# plot(eta_list,total_cost_list_random_highpen,color='cyan',marker='v', fillstyle='none',linestyle='--',label="RND")
+plot(eta_list,total_cost_list_myopic_highpen,color='green',marker='^', fillstyle='none',linestyle='--',label="MYO")
+subplots_adjust(top=0.93,bottom=0.16,left=0.12, right=0.95)
+legend(loc='best')
+xlabel('Cloudlet availability $\\eta_a$', fontsize=16)
+ylabel('User\'s avg. cost', fontsize=16)
+ylim([95,145])
+locs, labels = plt.yticks()
+plt.setp(labels, rotation=90)
 
 show()
 print "TERMINATED."
